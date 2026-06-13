d229435caea2ea57a1497fdf9793636fa079407c
Alejandro Forero Cuervo <alefore@gmail.com>
2026-06-13 22:29:56 +0200

    More documentation for interface.

--- src/filter.ts
+++ src/filter.ts
@@ -11,6 +11,10 @@ export interface ImageFilter {
 }
 
 export interface ImageFilterFactory {
+  // Creates a new ImageFilter. On `update`, will read from `inputMat`, apply a
+  // filter, and write the output to `outputMat`. If it needs to display input
+  // points, displays them in inputCanvas. If it needs to add input elements (to
+  // configure filter parameters), adds them to parametersContainer.
   install(
       parametersContainer: HTMLElement, inputCanvas: HTMLCanvasElement,
       inputMat: any, outputMat: any, onUpdate: () => void): ImageFilter;
